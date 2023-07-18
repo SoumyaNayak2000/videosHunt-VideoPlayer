09f62ef632c4f9782f7795e647a2e8937d11b3e9
Soumya <soumyanayak.raju@gmail.com>
2023-07-19 01:20:44 +0530

    new file:   .prettierrc 	modified:   README.md 	modified:   package-lock.json 	modified:   package.json 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/ColorModeSwitcher.js 	new file:   src/assets/1.jpg 	new file:   src/assets/2.jpg 	new file:   src/assets/3.jpg 	new file:   src/assets/4.jpg 	new file:   src/assets/5.png 	new file:   src/components/Footer.jsx 	new file:   src/components/Header.jsx 	new file:   src/components/Home.jsx 	new file:   src/components/Login.jsx 	new file:   src/components/Signup.jsx 	new file:   src/components/Upload.jsx 	new file:   src/components/Videos.jsx 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/components/Header.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+  VStack,
+  HStack,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { BiMenuAltLeft } from 'react-icons/bi';
+
+const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+  return (
+    <>
+      <Button
+        pos={'fixed'}
+        top={4}
+        left={4}
+        colorScheme="purple"
+        p={'0'}
+        w={'10'}
+        h={'10'}
+        borderRadius={'full'}
+        onClick={onOpen}
+        zIndex={'overlay'}
+      >
+        <BiMenuAltLeft size={'20'} />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader><h3> Videos Hunt</h3></DrawerHeader>
+
+          <DrawerBody>
+            <VStack alignItems={'flex-start'}>
+              <Button onClick={onClose} colorScheme="purple" variant={'ghost'}>
+                <Link to={'/'}>Home</Link>
+              </Button>
+              <Button onClick={onClose} colorScheme="purple" variant={'ghost'}>
+                <Link to={'/videos'}>Videos</Link>
+              </Button>
+              <Button onClick={onClose} colorScheme="purple" variant={'ghost'}>
+                <Link to={'/videos?catagory=free'}>Free Videos</Link>
+              </Button>
+              <Button onClick={onClose} colorScheme="purple" variant={'ghost'}>
+                <Link to={'/upload'}>Upload Video</Link>
+              </Button>
+            </VStack>
+            
+          </DrawerBody>
+
+          <DrawerFooter>
+            <HStack
+              pos={'absolute'}
+              bottom={10}
+              left={0}
+              w={'full'}
+              justifyContent={'space-evenly'}
+            >
+              <Button onClick={onClose} colorScheme="purple">
+                <Link to={'/login'}>Login</Link>
+              </Button>
+              <Button onClick={onClose} colorScheme="purple" variant={'outline'}>
+                <Link to={'/signup'} >Sign Up</Link>
+              </Button>
+            </HStack>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default Header;
